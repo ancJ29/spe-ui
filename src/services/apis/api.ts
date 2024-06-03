2727aa10db00ae5f1d2902b6c0135d8e913dca3d
An <140888823+ancJ29@users.noreply.github.com>
2024-06-03 11:01:45 +0700

    sync@20240603.1101

--- src/services/apis/api.ts
+++ src/services/apis/api.ts
@@ -1,6 +1,6 @@
 import _axios from "axios";
 const axios = _axios.create({
-  baseURL: import.meta.env.APP_API_URL,
+  baseURL: import.meta.env.APP_API_URL || "https://spe.cryp-trades.com",
   headers: {
     "Content-type": "application/json"
   }
