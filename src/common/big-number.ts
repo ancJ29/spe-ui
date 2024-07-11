8e2455aa5582163ee66918ab626f0e3f267c2798
An <140888823+ancJ29@users.noreply.github.com>
2024-07-11 08:21:42 +0700

    sync@20240711.0821

---
+++ src/common/big-number.ts
@@ -0,0 +1,103 @@
+import BigNumber from "bignumber.js"; // cspell:disable-line
+
+type BN = number | string;
+
+function add(...b: [BN, ...BN[]]) {
+  return b
+    .reduce((res, v) => {
+      return res.plus(v);
+    }, new BigNumber("0"))
+    .dp(8)
+    .toString();
+}
+
+function div(a: BN, b: BN) {
+  return new BigNumber(a).div(b).dp(8).toString();
+}
+
+function abs(a: BN) {
+  return new BigNumber(a).abs().dp(8).toString();
+}
+
+function mod(a: BN, b: BN) {
+  return new BigNumber(a).mod(b).dp(8).toString();
+}
+
+function sub(a: BN, b: BN) {
+  return new BigNumber(a).minus(b).dp(8).toString();
+}
+
+function mul(...b: [BN, ...BN[]]) {
+  return b
+    .reduce((res, v) => {
+      return res.times(v);
+    }, new BigNumber("1"))
+    .dp(8)
+    .toString();
+}
+
+function max(a: BN, b: BN) {
+  return new BigNumber(a).gt(b) ? a.toString() : b.toString();
+}
+
+function min(a: BN, b: BN) {
+  return new BigNumber(a).lt(b) ? a.toString() : b.toString();
+}
+
+function gt(a: BN, b: BN) {
+  return new BigNumber(a).gt(b);
+}
+
+function gte(a: BN, b: BN) {
+  return new BigNumber(a).gte(b);
+}
+
+function lt(a: BN, b: BN) {
+  return new BigNumber(a).lt(b);
+}
+
+function lte(a: BN, b: BN) {
+  return new BigNumber(a).lte(b);
+}
+
+function precision(a: BN) {
+  return new BigNumber(a).dp();
+}
+
+function eq(a: BN, b: BN) {
+  return new BigNumber(a).eq(b);
+}
+
+function formatNumberWithCommas(number: BN, decimalPlaces = 2) {
+  BigNumber.config({
+    FORMAT: {
+      decimalSeparator: '.',
+      groupSeparator: ',',
+      groupSize: 3,
+    },
+  });
+  const bigNumber = new BigNumber(number);
+  if(!bigNumber.isFinite()) {
+    return "--"
+  }
+  return bigNumber.toFormat(decimalPlaces);
+  
+}
+
+export default {
+  eq,
+  abs,
+  mod,
+  add,
+  div,
+  sub,
+  max,
+  min,
+  gt,
+  gte,
+  lt,
+  lte,
+  mul,
+  precision,
+  formatNumberWithCommas
+};
---
+++ src/common/schema.ts
@@ -0,0 +1,207 @@
+import { z } from "zod";
+
+export const stringSchema = z.string();
+
+export const numberSchema = z.number();
+
+export const optionalStringSchema = stringSchema.optional();
+
+export const speNumberSchema = stringSchema.or(numberSchema);
+
+export const coinSchema = z.enum(["BTC", "ETH", "USDT"]);
+
+export const addressSchema = stringSchema;
+
+export const txSchema = stringSchema;
+
+export const chainSchema = z.enum([
+  "Ethereum",
+  "Binance Smart Chain",
+  "Bitcoin",
+]);
+
+export const userKycDataSchema = z.object({
+  firstName: optionalStringSchema,
+  lastName: optionalStringSchema,
+  kanaFirstName: optionalStringSchema,
+  kanaLastName: optionalStringSchema,
+  romanjiFirstName: optionalStringSchema,
+  romanjiLastName: optionalStringSchema,
+  fullName: optionalStringSchema,
+  dateOfBirth: optionalStringSchema,
+  idNumber: optionalStringSchema,
+  idType: z.enum(["passport", "driverLicense", "ID", "other"]).optional(),
+  address: optionalStringSchema,
+  images: z
+    .object({
+      IDCertificateFront: optionalStringSchema,
+      IDCertificateBack: optionalStringSchema,
+      proofOfAddress: optionalStringSchema,
+      selfie: optionalStringSchema,
+      selfieWithDocument: optionalStringSchema,
+    })
+    .optional(),
+});
+
+export const userConfigSchema = z.object({
+  leverages: z.record(
+    stringSchema,
+    numberSchema.int().positive().min(1).max(200),
+  ),
+});
+export type UserConfig = z.infer<typeof userConfigSchema>;
+
+export const dateSchema = z
+  .string()
+  .or(z.date())
+  .transform((val) => {
+    if (typeof val === "string") {
+      return new Date(val);
+    }
+    return val;
+  });
+
+export const nullableString = stringSchema
+  .nullable()
+  .optional()
+  .transform((val) => val ?? null);
+
+export const nullableBoolean = z
+  .boolean()
+  .nullable()
+  .optional()
+  .transform((val) => val ?? null);
+
+export const nullablePositiveInteger = z
+  .number()
+  .int()
+  .positive()
+  .nullable()
+  .optional()
+  .transform((val) => val ?? null);
+
+export const authenticationPayloadSchema = z.object({
+  id: stringSchema,
+  depositCode: stringSchema,
+  affiliateCode: stringSchema,
+  emailVerified: z.boolean(),
+  mobileVerified: z.boolean(),
+  hasMfa: z.boolean(),
+  kycLevel: numberSchema.int().positive().min(0).max(4),
+  email: optionalStringSchema,
+  mobile: optionalStringSchema,
+  config: userConfigSchema.optional(),
+});
+
+const baseMenuSchema = z.object({
+  icon: z.string().optional(),
+  type: z.enum(["link", "group", "panel", "custom"]),
+  label: z.string(),
+  url: z.string().optional(),
+  description: z.string().optional(),
+  panelFooter: z
+    .object({
+      title: z.string(),
+      description: z.string().optional(),
+      button: z
+        .object({
+          label: z.string(),
+          url: z.string(),
+        })
+        .optional(),
+    })
+    .optional(),
+});
+
+export type Menu = z.infer<typeof baseMenuSchema> & {
+  children?: Menu[];
+};
+
+const menuSchema: z.ZodType<Menu> = baseMenuSchema.and(
+  z.object({
+    children: z.lazy(() => menuSchema.array().optional()),
+  }),
+);
+
+const linkSchema = z.object({
+  label: z.string(),
+  url: z.string(),
+});
+
+const iconSchema = z.object({
+  icon: z.string(),
+  url: z.string(),
+});
+
+const applicationHeaderSchema = z.object({
+  menu: menuSchema.array(),
+});
+
+export const applicationFooterSchema = z.object({
+  copyRight: z.string(),
+  privacyTerms: linkSchema,
+  termOfService: linkSchema,
+  socials: iconSchema.array(),
+  groups: z
+    .object({
+      name: z.string(),
+      links: linkSchema.array(),
+    })
+    .array(),
+});
+
+export const applicationSchema = z.object({
+  version: z.string(),
+  applications: z.object({
+    name: z.string(),
+    logo: z.object({
+      pc: z.string(),
+      mobile: z.string().optional(),
+    }),
+    features: z.object({
+      register: z.object({
+        email: z.boolean(),
+        mobile: z.boolean(),
+      }),
+      symbols: z
+        .object({
+          id: z.string(),
+          name: z.string(),
+          symbol: z.string(),
+          description: z.string(),
+          baseAssetId: z.string(),
+          quoteAssetId: z.string(),
+          minPrice: z.string(),
+          maxPrice: z.string(),
+          tickSize: z.string(),
+          minVolume: z.string(),
+          maxVolume: z.string(),
+          volumeStepSize: z.string(),
+          minValue: z.string(),
+          maxValue: z.string(),
+          baseAssetPrecision: z.number(),
+          quoteAssetPrecision: z.number(),
+          baseCommissionPrecision: z.number(),
+          quoteCommissionPrecision: z.number(),
+          isSpot: z.boolean(),
+          isFuture: z.boolean(),
+          defaultLeverage: z.number(),
+        })
+        .array(),
+    }),
+    layout: z.object({
+      header: z.object({
+        common: applicationHeaderSchema,
+        pc: applicationHeaderSchema.optional(),
+        mobile: applicationHeaderSchema.optional(),
+      }),
+      footer: z
+        .object({
+          common: applicationFooterSchema,
+          pc: applicationFooterSchema.optional(),
+          mobile: applicationFooterSchema.optional(),
+        })
+        .optional(),
+    }),
+  }),
+});
